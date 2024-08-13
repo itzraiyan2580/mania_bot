d5fa08ad6e547d1e32d3ee1788c85afcf9158fea
itzraiyan2580 <112202419+itzraiyan2580@users.noreply.github.com>
2024-08-13 11:49:14 +0600

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ except ValueError:
 FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Welcome, {mention} !!</b><i> To continue, please subscribe to our channel ‼️</i></b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
-CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", "<code>{filename}</code> <b>@Anime_mania_0</b>")
+CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 
 #set True if you want to prevent users from forwarding files from bot
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" else False
