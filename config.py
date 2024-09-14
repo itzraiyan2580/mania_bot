8ef728df6126ef29317fa236e5eea4d70deb3526
itzraiyan2580 <112202419+itzraiyan2580@users.noreply.github.com>
2024-09-14 17:33:46 +0600

    Update config.py

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ APP_ID = int(os.environ.get("APP_ID", "26684254"))
 API_HASH = os.environ.get("API_HASH", "fc836096a68be3a4fcd7594cb3d9326f")
 
 #Your db channel Id
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002249418205"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002167789493"))
 
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6161189904"))
@@ -28,8 +28,8 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "6161189904"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://narutoanimemania:naruto106@atlascluster.onfsb.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-DB_NAME = os.environ.get("DATABASE_NAME", "sample_mflix")
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://narutouzumaki22551:narutouzumaki22551@cluster0.econe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002026477147"))
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002026477147"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "<b>Hello, {mention} 👋</b>\n\n<b> I'm a permanent file store bot for </b><b>@anime_mania_0</b><b> &amp; </b><b>@anime_flix_0</b><b>...🍷</b><b> To use me, Join the channels🖤</b>")
+START_MSG = os.environ.get("START_MESSAGE", "<b>Hello, {mention} 👋</b>\n\n<b> I'm a permanent Post & Files store bot for </b><b>@Anime_mania_0</b><b> &amp; </b><b>@anime_flix_0</b><b>...🍷</b><b> To use me, Join the channels🖤</b>")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -46,7 +46,7 @@ except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
 
 #Force sub message 
-FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello, {mention} 👋</b> <b><i> To continue, please subscribe to our channel ‼️</i></b>")
+FORCE_MSG = os.environ.get("FORCE_SUB_MESSAGE", "<b>Hello, {mention} 👋</b>\n\n<b>To continue, Please subscribe to our channel ‼️</b>")
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
@@ -55,10 +55,10 @@ CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
 PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" else False
 
 #Set true if you want Disable your Channel Posts Share button
-DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "False") == 'True'
+DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", "True") == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "<b>❌Don't send me messages directly I'm only A File Share bot!</b>"
+USER_REPLY_TEXT = "<b>❌Don't send me messages directly I'm only A File Sharing bot!</b>"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
